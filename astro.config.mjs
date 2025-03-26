32b3d0d0fdecffe3327bf71fd1bd2c98a4f3b804
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2025-03-26 15:41:45 -0300

    Update site URL and base path in astro.config for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dragneel98.github.io',
+    site: 'https://dragneel98.github.io/astro-github-pages/',
     base: '/astro-github-pages/',
-    // output: 'static',
+    output: 'static',
 });
